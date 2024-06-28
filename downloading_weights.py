726ba195226d0b2269ff221194995f817b420632
岳坤 <yuekundev@163.com>
2024-06-28 14:44:17 +0800

    修改：下载

--- downloading_weights.py
+++ downloading_weights.py
@@ -1,38 +1,63 @@
 import os
 import wget
 from tqdm import tqdm
+from huggingface_hub import hf_hub_download
 
-os.makedirs('pretrained_weights', exist_ok=True)
+def download_models(model_dir: str = 'pretrained_weights'):
+    os.makedirs(model_dir, exist_ok=True)
 
-urls = ['https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_l_8x8_300e_coco/yolox_l_8x8_300e_coco_20211126_140236-d3bd2b23.pth',
-        'https://huggingface.co/yzd-v/DWPose/resolve/main/dw-ll_ucoco_384.pth',
-        'https://huggingface.co/TMElyralab/MusePose/resolve/main/MusePose/denoising_unet.pth',
-        'https://huggingface.co/TMElyralab/MusePose/resolve/main/MusePose/motion_module.pth',
-        'https://huggingface.co/TMElyralab/MusePose/resolve/main/MusePose/pose_guider.pth',
-        'https://huggingface.co/TMElyralab/MusePose/resolve/main/MusePose/reference_unet.pth',
-        'https://huggingface.co/lambdalabs/sd-image-variations-diffusers/resolve/main/unet/diffusion_pytorch_model.bin',
-        'https://huggingface.co/lambdalabs/sd-image-variations-diffusers/resolve/main/image_encoder/pytorch_model.bin',
-        'https://huggingface.co/stabilityai/sd-vae-ft-mse/resolve/main/diffusion_pytorch_model.bin'
-        ]
+    urls = [
+        'https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_l_8x8_300e_coco/yolox_l_8x8_300e_coco_20211126_140236-d3bd2b23.pth',
+        # Hugging Face model identifiers
+        ('yzd-v/DWPose', 'dw-ll_ucoco_384.pth'),
+        ('TMElyralab/MusePose', 'MusePose/denoising_unet.pth'),
+        ('TMElyralab/MusePose', 'MusePose/motion_module.pth'),
+        ('TMElyralab/MusePose', 'MusePose/pose_guider.pth'),
+        ('TMElyralab/MusePose', 'MusePose/reference_unet.pth'),
+        ('lambdalabs/sd-image-variations-diffusers', 'unet/diffusion_pytorch_model.bin'),
+        ('lambdalabs/sd-image-variations-diffusers', 'image_encoder/pytorch_model.bin'),
+        ('stabilityai/sd-vae-ft-mse', 'diffusion_pytorch_model.bin')
+    ]
 
-paths = ['dwpose', 'dwpose', 'MusePose', 'MusePose', 'MusePose', 'MusePose', 'sd-image-variations-diffusers/unet', 'image_encoder', 'sd-vae-ft-mse']
+    paths = [
+        'dwpose', 'dwpose', '', '', '', '', 
+        'sd-image-variations-diffusers', '', 'sd-vae-ft-mse'
+    ]
 
-for path in paths:
-  os.makedirs(f'pretrained_weights/{path}', exist_ok=True)
+    for path in paths:
+        dir = os.path.join(model_dir, path)
+        os.makedirs(dir, exist_ok=True)
 
-# saving weights 
-for url, path in tqdm(zip(urls, paths)):
-    filename = wget.download(url, f'pretrained_weights/{path}')
+    for url, path in tqdm(zip(urls, paths)):
+        if isinstance(url, tuple):
+            repo_id, filename = url
+            full_file_path = os.path.join(model_dir, path, filename)
+            if not os.path.exists(full_file_path):
+                print(f"Model '{filename}' does not exist. Downloading to '{full_file_path}'..")
+                hf_hub_download(repo_id=repo_id, filename=filename, local_dir=os.path.join(model_dir, path))
+        else:
+            filename = os.path.basename(url)
+            if filename == "yolox_l_8x8_300e_coco_20211126_140236-d3bd2b23.pth":
+                filename = "yolox_l_8x8_300e_coco.pth"
+            full_file_path = os.path.join(model_dir, path, filename)
+            if not os.path.exists(full_file_path):
+                print(f"Model '{filename}' does not exist. Downloading to '{full_file_path}'..")
+                wget.download(url, full_file_path)
 
-config_urls = ['https://huggingface.co/lambdalabs/sd-image-variations-diffusers/resolve/main/unet/config.json',
-           'https://huggingface.co/lambdalabs/sd-image-variations-diffusers/resolve/main/image_encoder/config.json',
-           'https://huggingface.co/stabilityai/sd-vae-ft-mse/resolve/main/config.json']
+    config_urls = [
+        ('lambdalabs/sd-image-variations-diffusers', 'unet/config.json'),
+        ('lambdalabs/sd-image-variations-diffusers', 'image_encoder/config.json'),
+        ('stabilityai/sd-vae-ft-mse', 'config.json')
+    ]
 
-config_paths = ['sd-image-variations-diffusers/unet', 'image_encoder', 'sd-vae-ft-mse']
+    config_paths = ['sd-image-variations-diffusers', '', 'sd-vae-ft-mse']
 
-# saving config files 
-for url, path in tqdm(zip(config_urls, config_paths)):
-    filename = wget.download(url, f'pretrained_weights/{path}')
+    for url, path in tqdm(zip(config_urls, config_paths)):
+        repo_id, filename = url
+        full_file_path = os.path.join(model_dir, path, filename)
+        if not os.path.exists(full_file_path):
+            print(f"Config '{filename}' does not exist. Downloading to '{full_file_path}'..")
+            hf_hub_download(repo_id=repo_id, filename=filename, local_dir=os.path.join(model_dir, path))
 
-# renaming model name as given in readme
-os.rename('pretrained_weights/dwpose/yolox_l_8x8_300e_coco_20211126_140236-d3bd2b23.pth', 'pretrained_weights/dwpose/yolox_l_8x8_300e_coco.pth')
+# 运行下载函数
+download_models()
